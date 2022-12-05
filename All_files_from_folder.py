a620397c814abe15a2de22e035bbb2d0253112c0
Flotfar <113911826+Flotfar@users.noreply.github.com>
2022-12-05 08:37:11 +0100

    Update All_files_from_folder.py

--- All_files_from_folder.py
+++ All_files_from_folder.py
@@ -8,8 +8,10 @@ folder = r'/Users/joakimbonde/Desktop/File_test/'   # file path, rapped with 'r'
 file_num = "HBA.2.9_1_"                             # file initial numbering
 
 
+#########################################################
+#### Creating Excel file for exportation of results: ####
+#########################################################
 
-# Creating Excel file for exportation of results:
 workbook = xlsxwriter.Workbook('File_info.xlsx')
 worksheet = workbook.add_worksheet('Names')
 
@@ -21,6 +23,9 @@ worksheet.write(0, 0, "Old_names:" , title_format)
 worksheet.write(0, 1, "New names:" , title_format)
 
 
+#########################################################
+#### Extracting and renaming files:                  ####
+#########################################################
 
 row = 1
 # count increase by 1 in each iteration
