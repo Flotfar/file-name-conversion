fcf539f281e04324cdd1b4844269b51d6a3898eb
Flotfar <113911826+Flotfar@users.noreply.github.com>
2023-01-29 18:49:34 +0100

    Create File_name_matrix.py

---
+++ File_name_matrix.py
@@ -0,0 +1,8 @@
+# [Filename, Rev, Date]
+
+A = [
+    ['Morten.Andersen01.pdf' , 'Morten', '10', "01-10-2019"], 
+    ['Peter_milchansen-51.pdf' , 'Peter', '0', "10-01-2020"],
+    ['Hanne 20 mandaley/who.pdf' , 'Hanne', '10', "01-10-2019"],
+    ['Morten.Mortensen.docx' , 'Morten2', '10', "01-10-2019"]
+]
